1a238885faa4e40ba95b3d62a478f41fe1527c80
agustinv2018 <agustin.v420@gmail.com>
2022-09-29 23:02:02 -0300

    resolucion ejercicio

--- acciones/siniestros/nuevo.php
+++ acciones/siniestros/nuevo.php
@@ -4,6 +4,8 @@ header('Content-Type: application/json');
 
 require_once 'responses/nuevoResponse.php';
 require_once 'request/nuevoRequest.php';
+require_once '../../modelo/medioscontacto.php';
+require_once '../../modelo/vehiculo.php';
 
 $json = file_get_contents('php://input', true);
 $req = json_decode($json);
@@ -14,37 +16,34 @@ $resp->IsOk = true;
 
 if ($req->NroPoliza > 1000 or $req->NroPoliza < 0) {
     $resp->IsOk = false;
-    $resp->Mensaje = 'La póliza no existe';
+    $resp->Mensaje[] = 'La póliza no existe';
+} else {
+    if ($req->Vehiculo == null) {
+        $resp->IsOk = false;
+        $resp->Mensaje[] = 'Debe indicar el vehiculo';
+    }
+
+    if ($req->Vehiculo->Marca == null or $req->Vehiculo->Modelo == null or $req->Vehiculo->Version == null or $req->Vehiculo->Anio == null) {
+        $resp->IsOk = false;
+        $resp->Mensaje[] = 'Debe indicar todas las propiedades del vehiculo';
+    }
+
+    $cantmedios = 0;
+
+    foreach ($req->ListMediosContacto as $c) {
+        $cantmedios =  $cantmedios + 1;
+    }
+    if ($cantmedios <= 0) {
+        $resp->IsOk = false;
+        $resp->Mensaje = 'Debe indicar al menos un medio de contacto';
+    }
+
+    foreach ($req->ListMediosContacto as $mc) {
+
+        if ($mc->MedioContactoDescripcion != 'Celular' && $mc->MedioContactoDescripcion != 'Email') {
+            $resp->IsOk = false;
+            $resp->Mensaje[] = 'Debe indicar medios de contacto válidos';
+        }
+    }
 }
-
-if ($req->Vehiculo = null) {
-    $resp->IsOk = false;
-    $resp->Mensaje = 'Debe indicar el vehiculo';
-}
-
-if ($req->Vehiculo->Marca = null or $req->Vehiculo->Modelo = null or $req->Vehiculo->Version = null or $req->Vehiculo->Anio = null) {
-    $resp->IsOk = false;
-    $resp->Mensaje = 'Debe indicar todas las propiedades del vehiculo';
-}
-
-$cantmedios = 0;
-
-foreach ($req->ListMediosContacto as $c) {
-    $cantmedios =  $cantmedios + 1;
-}
-if ($cantmedios <= 0) {
-    $resp->IsOk = false;
-    $resp->Mensaje = 'Debe indicar al menos un medio de contacto';
-}
-
-$m1 = new MediosContacto();
-$m1->MedioContactoDescripcion = 'Celular';
-$m1->Valor = '';
-
-$m2 = new MediosContacto();
-$m2->MedioContactoDescripcion = 'Mail';
-$m2->Valor = '';
-
-
-
-
+echo json_encode($resp);
